a24e394994c1b0861c0d8f1f5a18ac93eb65a47e
prodevhub <ebrand98@hotmail.com>
2016-05-30 11:18:26 +0200

    new lesson

--- GradeManager/app/src/main/java/ch/bbcag/blugij/grademanager/activity/EditNoteActivity.java
+++ GradeManager/app/src/main/java/ch/bbcag/blugij/grademanager/activity/EditNoteActivity.java
@@ -36,8 +36,7 @@ public class EditNoteActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
     }
