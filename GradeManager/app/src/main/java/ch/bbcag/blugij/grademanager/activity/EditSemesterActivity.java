416244bfed31da7471fdca4640eacc95caab84c6
prodevhub <ebrand98@hotmail.com>
2016-06-06 13:51:38 +0200

    duplicate semester on create

--- GradeManager/app/src/main/java/ch/bbcag/blugij/grademanager/activity/EditSemesterActivity.java
+++ GradeManager/app/src/main/java/ch/bbcag/blugij/grademanager/activity/EditSemesterActivity.java
@@ -10,10 +10,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import ch.bbcag.blugij.grademanager.R;
+import ch.bbcag.blugij.grademanager.adapter.FachAdapter;
+import ch.bbcag.blugij.grademanager.adapter.SemesterAdapter;
 import ch.bbcag.blugij.grademanager.sqlite.helper.DatabaseHelper;
 import ch.bbcag.blugij.grademanager.sqlite.model.Semester;
 import ch.bbcag.blugij.grademanager.utils.UIHelper;
@@ -24,6 +30,8 @@ public class EditSemesterActivity extends AppCompatActivity {
     public static final String INTENT_EXTRA_SEMESTER_ID = "edit_semester_semester_id";
     private boolean isEdit = false;
     private Semester editSemester;
+    private boolean duplicate = false;
+    private Semester oldSemester;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,8 +79,13 @@ public class EditSemesterActivity extends AppCompatActivity {
                             editSemester.setBezeichnung(bezeichnung);
                             databaseHelper.updateSemester(editSemester);
                         } else {
-                            Semester semester = new Semester(bezeichnung, 0.0);
-                            databaseHelper.createSemester(semester);
+                            if (duplicate){
+                                Spinner spinnerSemester = (Spinner) findViewById(R.id.edit_semester_spinner);
+                                databaseHelper.duplicateSemester(oldSemester, bezeichnung);
+                            } else {
+                                Semester semester = new Semester(bezeichnung, 0.0);
+                                databaseHelper.createSemester(semester);
+                            }
                         }
 
                         if (hasSame) {
@@ -106,5 +119,51 @@ public class EditSemesterActivity extends AppCompatActivity {
 
             editText.setText(editSemester.getBezeichnung());
         }
+
+        CheckBox cbxFaecherUebernehmen = (CheckBox) findViewById(R.id.cbx_faecher_uebernehmen);
+        final Spinner spinnerSemester = (Spinner) findViewById(R.id.edit_semester_spinner);
+
+
+        SemesterAdapter semesterAdapter = new SemesterAdapter(this, R.layout.custom_list_view_item_one_column, databaseHelper.getAllSemesters(), true);
+        spinnerSemester.setAdapter(semesterAdapter);
+        spinnerSemester.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                oldSemester = (Semester) parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                oldSemester = null;
+            }
+        });
+
+        if (databaseHelper.getAllSemesters().size() < 1){
+            cbxFaecherUebernehmen.setEnabled(false);
+        } else {
+            cbxFaecherUebernehmen.setEnabled(true);
+        }
+
+        cbxFaecherUebernehmen.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    spinnerSemester.setVisibility(View.VISIBLE);
+                    duplicate = true;
+                } else {
+                    spinnerSemester.setVisibility(View.INVISIBLE);
+                    duplicate = false;
+                }
+            }
+        });
+
+        spinnerSemester.setVisibility(View.INVISIBLE);
+        cbxFaecherUebernehmen.setChecked(false);
+
+        if (isEdit){
+            cbxFaecherUebernehmen.setVisibility(View.INVISIBLE);
+            spinnerSemester.setVisibility(View.INVISIBLE);
+        }
     }
+
 }
